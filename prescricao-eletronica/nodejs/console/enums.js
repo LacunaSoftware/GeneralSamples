938194f9a023b0d0b0080772cdadab9dfe624d1b
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2020-06-17 20:27:06 -0300

    [prescricao-eletronica] Recovered node project files.

---
+++ prescricao-eletronica/nodejs/console/enums.js
@@ -0,0 +1,23 @@
+const DocumentType = {
+    PrescricaoMedicamento: "2_16_76_1_12_1_1",
+    AtestadoMedico: "2_16_76_1_12_1_2",
+    SolicitacaoExame: "2_16_76_1_12_1_3",
+    LaudoLaboratorial: "2_16_76_1_12_1_4",
+    SumarioAlta: "2_16_76_1_12_1_5",
+    RegistroAtendimentoClinico: "2_16_76_1_12_1_6",
+    DispensacaoMedicamento: "2_16_76_1_12_1_7",
+    Vacinacao: "2_16_76_1_12_1_8",
+    RelatorioMedico: "2_16_76_1_12_1_11"
+};
+
+const FieldName = {
+    Crm: "2_16_76_1_4_2_2_1",
+    CrmUF: "2_16_76_1_4_2_2_2",
+    CrmEspecialidade: "2_16_76_1_4_2_2_3",
+    Crf: "2_16_76_1_4_2_3_1",
+    CrfUF: "2_16_76_1_4_2_3_1",
+    CrfEspecialidade: "2_16_76_1_4_2_3_1"
+};
+
+exports.DocumentType = DocumentType;
+exports.FieldName = FieldName;
